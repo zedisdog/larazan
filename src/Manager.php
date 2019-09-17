29d42bf7d161ffffb26b4865058c858212a6d7f2
zed <zedsmail@163.com>
2019-09-17 16:45:58 +0800

    comment

--- src/Manager.php
+++ src/Manager.php
@@ -23,36 +23,37 @@ use Psr\Log\LoggerInterface;
 /**
  * Class Manager
  * @package Dezsidog\LYouzanphp
- * @method string getClientId()
- * @method string getClientSecret()
- * @method bool addSalesmanAccount($identification,int $fans_type = 0,string $from_mobile = '',int $level = 0,int $group_id = 0,string $version = '3.0.1')
- * @method array|null refund(string $desc, string $oid, int $refundFee, string $tid, string $version = '3.0.0')
- * @method bool|null ticketVerify(array $params, $version = '1.0.0')
- * @method array|null getTrade(string $tid, string $version = '4.0.0')
- * @method bool|null ticketCreate(string $tickets, string $orderNo, int $singleNum = 1, string $version = '1.0.0')
- * @method bool|null pointIncrease(string $accountId, int $accountType, int $points, string $reason, string $bizValue = '', string $version = '3.1.0')
- * @method array|null getSalesman(string|int $identification, string $version = '3.0.1')
- * @method string|null getPhoneByTrade(string $tradeId, string $version = '3.0.0')
- * @method array|null addTags(int $id, string $tags, $version = '3.0.0')
- * @method array|null itemSearch(string $keyword,int $pageNo = 1,int $pageSize = 100,int $showSoldOut = 2,$version = '3.0.0')
- * @method array|null itemListByItemIds(array $item_ids,int $page_no = 1,int $page_size = 100,int $show_sold_out = 2,$version = '3.0.0')
- * @method bool|null addCostumerTags(string $accountType, string $accountId, array $tags, string $version = '4.0.0')
- * @method array|null getOpenIdByPhone(string $mobile, string $countryCode = '86', string $version = '3.0.0')
- * @method array|null getShopInfo(string $version = '3.0.0')
- * @method array|null getItemCategories(string $version = '3.0.0')
- * @method array|null getOnSaleItems(int $pageNo = 1,int $pageSize = 40,string $q = '',int $tagId = 0,?Carbon $updateTimeStart = null,?Carbon $updateTimeEnd = null,string $orderBy = 'created_time:desc',string $version = '3.0.0')
- * @method array|null getInventoryItems(int $pageNo = 1,int $pageSize = 40,string $banner = '',string $q = '',int $tagId = 0,?Carbon $updateTimeStart = null,?Carbon $updateTimeEnd = null,string $orderBy = 'created_time:desc',string $version = '3.0.0')
- * @method array|null getProducts(int $pageNo = 1,int $pageSize = 40,string $itemIds = '',int $showSoldOut = 2,string $q = '',string $tagIds = '',string $version = '3.0.0')
- * @method array|null getShopBaseInfo(string $version = '3.0.0')
- * @method array|null givePresent(int $activityId, int $fansId, int $buyerId = 0, string $version = '3.0.0')
- * @method array|null getPresents(string $version = '3.0.0')
- * @method array|null getUnfinishedCoupons(string $fields = '', string $version = '3.0.0')
- * @method array|null getCoupon(int $id, string $version='3.0.0')
- * @method array|null takeCoupon(int $couponGroupId, string $identify, string $type, $version='3.0.0')
- * @method array|null getCouponList(string $groupType, string $status, int $pageNo = 1, int $pageSize = 1000, string $version = '3.0.0')
- * @method array|null getSalesmanList($pageNo = 1, $pageSize = 100, $version = '3.0.0')
- * @method array|null itemGet($identification, $alias = false, string $version = '3.0.0')
- * @method array|null getFollower($id, string $version='3.0.0')
+ * @method string getClientId() 获取client_id
+ * @method string getClientSecret() 获取client_secret
+ * @method bool itemDelete($item_id, string $yz_open_id = '', $version = '3.0.1') 删除商品
+ * @method bool addSalesmanAccount($identification,int $fans_type = 0,string $from_mobile = '',int $level = 0,int $group_id = 0,string $version = '3.0.1') 添加分销员
+ * @method array|null refund(string $desc, string $oid, int $refundFee, string $tid, string $version = '3.0.0') 主动退款
+ * @method bool|null ticketVerify(array $params, $version = '1.0.0') 核销
+ * @method array|null getTrade(string $tid, string $version = '4.0.0') 获取订单详情
+ * @method bool|null ticketCreate(string $tickets, string $orderNo, int $singleNum = 1, string $version = '1.0.0') 创建核销码
+ * @method bool|null pointIncrease(string $accountId, int $accountType, int $points, string $reason, string $bizValue = '', string $version = '3.1.0') 加积分
+ * @method array|null getSalesman(string|int $identification, string $version = '3.0.1') 获取分销员详情
+ * @method string|null getPhoneByTrade(string $tradeId, string $version = '3.0.0') 通过订单id获取分销员手机号
+ * @method array|null addTags(int $id, string $tags, $version = '3.0.0') 向用户添加tag
+ * @method array|null itemSearch(string $keyword,int $pageNo = 1,int $pageSize = 100,int $showSoldOut = 2,$version = '3.0.0') 搜索商品
+ * @method array|null itemListByItemIds(array $item_ids,int $page_no = 1,int $page_size = 100,int $show_sold_out = 2,$version = '3.0.0') 根据商品id获取商品数组
+ * @method bool|null addCostumerTags(string $accountType, string $accountId, array $tags, string $version = '4.0.0') 向客户添加tag
+ * @method array|null getOpenIdByPhone(string $mobile, string $countryCode = '86', string $version = '3.0.0') 根据手机号码获取openId
+ * @method array|null getShopInfo(string $version = '3.0.0') 获取店铺信息
+ * @method array|null getItemCategories(string $version = '3.0.0') 获取商品分组
+ * @method array|null getOnSaleItems(int $pageNo = 1,int $pageSize = 40,string $q = '',int $tagId = 0,?Carbon $updateTimeStart = null,?Carbon $updateTimeEnd = null,string $orderBy = 'created_time:desc',string $version = '3.0.0') 获取上架的商品
+ * @method array|null getInventoryItems(int $pageNo = 1,int $pageSize = 40,string $banner = '',string $q = '',int $tagId = 0,?Carbon $updateTimeStart = null,?Carbon $updateTimeEnd = null,string $orderBy = 'created_time:desc',string $version = '3.0.0') 获取仓库中的商品
+ * @method array|null getProducts(int $pageNo = 1,int $pageSize = 40,string $itemIds = '',int $showSoldOut = 2,string $q = '',string $tagIds = '',string $version = '3.0.0') 获取所有商品，包括上架的和仓库中的
+ * @method array|null getShopBaseInfo(string $version = '3.0.0') 获取店铺基础信息
+ * @method array|null givePresent(int $activityId, int $fansId, int $buyerId = 0, string $version = '3.0.0') 向用户发送赠品
+ * @method array|null getPresents(string $version = '3.0.0') 获取进行中的赠品
+ * @method array|null getUnfinishedCoupons(string $fields = '', string $version = '3.0.0') 获取未结束的优惠活动
+ * @method array|null getCoupon(int $id, string $version='3.0.0') 获取优惠券详情
+ * @method array|null takeCoupon(int $couponGroupId, string $identify, string $type, $version='3.0.0') 发放优惠券/码
+ * @method array|null getCouponList(string $groupType, string $status, int $pageNo = 1, int $pageSize = 1000, string $version = '3.0.0') （分页查询）查询优惠券（码）活动列表
+ * @method array|null getSalesmanList($pageNo = 1, $pageSize = 100, $version = '3.0.0') 获取分销员列表
+ * @method array|null itemGet($identification, $alias = false, string $version = '3.0.0') 获取商品
+ * @method array|null getFollower($id, string $version='3.0.0') 通过 open_id 或者 fans_id 获取用户信息
  */
 class Manager
 {
