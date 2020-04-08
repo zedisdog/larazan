617ce09228cc714f84346eea0fbb150d8c42a849
zed <zedsmail@163.com>
2020-04-08 20:34:23 +0800

    docs: 新的订单消息格式注释

--- src/Message/TradeNormal.php
+++ src/Message/TradeNormal.php
@@ -6,6 +6,19 @@ namespace Dezsidog\Larazan\Message;
 
 use Illuminate\Support\Arr;
 
+/**
+ * Class TradeNormal
+ * @package Dezsidog\Larazan\Message
+ * @property-read string $tid 订单id
+ * @property-read string $status 定单状态
+ * @property-read Order[] $orders 子订单
+ * @property-read string $receiver_name 收货人姓名
+ * @property-read string $receiver_tel 收货人联系电话
+ * @property-read string $delivery_address 收货详细地址
+ * @property-read string $delivery_province 收货省
+ * @property-read string $delivery_city 收货市
+ * @property-read string $delivery_district 收货区
+ */
 class TradeNormal extends BaseMessageWithControlInfo
 {
     /**
